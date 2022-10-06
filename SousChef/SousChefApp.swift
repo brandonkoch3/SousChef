8329cab21545162eaa294353f659af883261d893
Brandon Koch <brandon.koch@nike.com>
2022-10-06 14:57:40 -0700

    Initial Commit

---
+++ SousChef/SousChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  SousChefApp.swift
+//  SousChef
+//
+//  Created by Brandon Koch on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SousChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
